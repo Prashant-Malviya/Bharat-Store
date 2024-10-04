01f02ac2225cdcdfec2a5caa40f81cea0a29a64c
Prashant Malviya <prashantmalviya272002@gmail.com>
2024-10-04 18:40:24 +0530

    code updated

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -1,12 +1,14 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
-import { BrowserRouter } from 'react-router-dom'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import { BrowserRouter } from "react-router-dom";
+import { ShopContext } from "./context/ShopContext.jsx";
 
-createRoot(document.getElementById('root')).render(
-  
+createRoot(document.getElementById("root")).render(
   <BrowserRouter>
-    <App />
+    <ShopContext>
+      <App />
+    </ShopContext>
   </BrowserRouter>
-)
+);
