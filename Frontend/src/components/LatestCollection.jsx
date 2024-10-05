a9f70d9c70a48dc267f4253858e95bffea5d84cf
Prashant Malviya <prashantmalviya272002@gmail.com>
2024-10-05 23:22:34 +0530

    code updated

--- Frontend/src/components/LatestCollection.jsx
+++ Frontend/src/components/LatestCollection.jsx
@@ -1,13 +1,28 @@
-import React, { useContext } from 'react'
+import React, { useContext, useEffect, useState } from 'react'
 import { ShopContext } from '../context/ShopContext'
+import Title from './Title';
 
 function LatestCollection() {
 
     const {products} = useContext(ShopContext);
+    const [latestProducts, setLatestProducts] = useState([]);
+
+    useEffect(()=>{
+      setLatestProducts(products.slice(0,10))
+    })
+
+    // console.log(products,'products');
+    
 
   return (
-    <div>
-      
+    <div className='my-10'>
+        <div className='text-center py-8 text-3xl'>
+          <Title text1={'LATEST'} text2={'COLLECTIONS'}/>
+          <p className='w-3/4 m-auto text-xs sm:text-sm md:text-base text-gray-600'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam vero iusto laudantium nemo.</p>
+        </div>
+
+
+
     </div>
   )
 }
