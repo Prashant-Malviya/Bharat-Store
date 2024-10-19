5960af2493291533b4ffc24a2aed449aa168d038
Prashant Malviya <prashantmalviya272002@gmail.com>
2024-10-19 23:28:31 +0530

    code updated

--- Backend/server.js
+++ Backend/server.js
@@ -1,11 +1,13 @@
 import express from "express";
 import cors from "cors";
 import 'dotenv/config'
+import connectDB from "./config/mongodb.js";
 
 
 // app config
 const app = express();
 const port = process.env.PORT || 4000;
+connectDB();
 
 //middlewares
 
@@ -17,5 +19,7 @@ app.use(cors());
 app.get("/", (req,res) => {
     res.send('<h1>Api is working</h1>');
 });
+
+//listening at port 4000
 
 app.listen(port,()=> console.log(`listening on localhost:${port}`));
